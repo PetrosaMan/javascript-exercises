aa293419e7a2c4143b0855bdbf2f6cd839653539
PetrosaMan <dw.todd.eng@gmail.com>
2024-09-08 16:40:47 +0200

    Completed first part of test 03_reverseString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,14 +2,12 @@ const repeatString = function(string, times) {
     if(times < 0) {
         return 'ERROR';
     }  
-
     let result = '';
     for(let j = 0; j < times; j++) {
         result += string;
     } 
     return result;   
 };
-
 
 // Do not edit below this line
 module.exports = repeatString;
