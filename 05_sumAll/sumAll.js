2d852b59b1b7d11798ef8bd031e5248c9589f04f
PetrosaMan <dw.todd.eng@gmail.com>
2024-09-12 20:45:30 +0200

    changed code to add Number.isInteger()

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,34 +1,24 @@
 const sumAll = function (int_1, int_2) {
+
+  if (!Number.isInteger(int_1) || !Number.isInteger(int_2)) return "ERROR";
   
-  if(int_1 < 0 || int_2 < 0) {    
-    return 'ERROR';
-  } 
-  
-  if(int_1 % 1 !== 0 || int_2 % 1 !== 0) {    
-    return 'ERROR';
+  if (int_1 < 0 || int_2 < 0) {
+    return "ERROR";
   }
-  
-  if( typeof int_1 === 'string' || typeof int_2 === 'string') {    
-    return 'ERROR';
-  }    
 
   let sum = 0;
   let min = int_1;
   let max = int_2;
-    
+  
   if (int_1 > int_2) {
     min = int_2;
     max = int_1;
   }
-
-  for( let i = min; i <= max; i++) {
+  for (let i = min; i <= max; i++) {
     sum += i;
   }
   return sum;
 };
-
-// See solution code for error checking using:
-// if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
 
 // Do not edit below this line
 module.exports = sumAll;
