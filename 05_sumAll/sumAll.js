c84772af3dd3b1174191cfe0a52e2744f589217b
PetrosaMan <dw.todd.eng@gmail.com>
2024-09-12 16:59:57 +0200

    Completed code for sumAll function. All tests passing

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,34 @@
-const sumAll = function() {
+const sumAll = function (int_1, int_2) {
+  
+  if(int_1 < 0 || int_2 < 0) {    
+    return 'ERROR';
+  } 
+  
+  if(int_1 % 1 !== 0 || int_2 % 1 !== 0) {    
+    return 'ERROR';
+  }
+  
+  if( typeof int_1 === 'string' || typeof int_2 === 'string') {    
+    return 'ERROR';
+  }    
 
+  let sum = 0;
+  let min = int_1;
+  let max = int_2;
+    
+  if (int_1 > int_2) {
+    min = int_2;
+    max = int_1;
+  }
+
+  for( let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 };
+
+//const total = sumAll(2, 4);
+//console.log(total);
 
 // Do not edit below this line
 module.exports = sumAll;
