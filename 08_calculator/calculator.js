7d74d9ccb23c94db656750767d0f0b8a88dd11dd
PetrosaMan <dw.todd.eng@gmail.com>
2024-09-13 22:28:14 +0200

    Added code for add, subtract, sum, multiply

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,18 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if(array.length === 0) {
+    return 0;
+  }
+
+  let sumOfElements = 0;
+	for(let i = 0; i < array.length; i++) {
+      sumOfElements += array[i];
+  }
+  return sumOfElements;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+    let product = 1; 
+    if(array.length < 2) { return 'ERROR'}
 
-};
+    for(let i = 0; i < array.length; i++) {
+        product *= array[i];
+    }
+    return product;
+  };
 
 const power = function() {
 	
@@ -21,6 +35,9 @@ const power = function() {
 const factorial = function() {
 	
 };
+
+// test functions with values
+//console.log(' product = ', multiply([1, 2, 3, 4]));
 
 // Do not edit below this line
 module.exports = {
