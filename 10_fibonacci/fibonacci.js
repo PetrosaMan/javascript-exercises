a7c88a0ed3ee8f668c786a00d52decc4c391d1ec
PetrosaMan <dw.todd.eng@gmail.com>
2024-09-17 15:27:47 +0200

    Completed code for fibonacci function, all tests passing

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+function fibonacci(fn) {
+  // fn = f(n-1) + f(n-2)
+  let a = 0;   
+  let b = 1;   
 
-};
+  if (fn < 0) {
+    return 'OOPS'
+  }
+  if (fn == 0) {
+    return 0;
+  }
+
+  for (let i = 2; i <= fn; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
