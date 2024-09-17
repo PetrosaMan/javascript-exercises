7e5f7295159877f537c743fe23a21ecd54b00f9d
PetrosaMan <dw.todd.eng@gmail.com>
2024-09-17 10:23:51 +0200

    Completed code for palindromes function. All tests are passing.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) { 
+  // Use a regex to filter out non alpha-numeric chars
+  const filteredString = string.replaceAll(/[^A-Za-z0-9]+/g,'').toLowerCase();    
+  const reversedString = [...filteredString].reverse().join('');
+  // return true if is palindrome else false
+  return (filteredString === reversedString); 
 };
 
 // Do not edit below this line
