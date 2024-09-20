7f1db6b6490279edaaa1f418c833a27aa0e23391
PetrosaMan <dw.todd.eng@gmail.com>
2024-09-20 12:07:18 +0200

    Added comments to the function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -22,13 +22,17 @@ const findTheOldest = function(people) {
            const date = new Date();
            person.yearOfDeath = date.getFullYear();           
         }       
-        return (person.yearOfDeath - person.yearOfBirth) > (acc.yearOfDeath - acc.yearOfBirth) ? person : acc;
-        console.log(person); 
+        return (person.yearOfDeath - person.yearOfBirth) > (acc.yearOfDeath - acc.yearOfBirth) ? person : acc;         
     }, people[0]);
   return oldestPerson;      
 };
 
-//console.log(findTheOldest(people));
+// Note: that this code passes all the tests.
+// See also under solution alternative coding of the function.
+// That function uses a helper function to calculate the age
+// of the current person, and also the accumulator person age
+// for comparison. This simplifys the findTheOldest by using the 
+// helper function. Though my code does pass all tests!!
 
 // Do not edit below this line
 module.exports = findTheOldest;
