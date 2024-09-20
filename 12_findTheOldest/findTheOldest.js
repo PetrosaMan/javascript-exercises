9ac4acb0031bc07804cb04b4217252ed417415f6
PetrosaMan <dw.todd.eng@gmail.com>
2024-09-20 10:01:37 +0200

    Added code to findTheOldest function. All tests are passing

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 2018,
+    yearOfDeath: 1970
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
 
+const findTheOldest = function(people) {  
+    const oldestPerson = people.reduce((acc, person) =>{             
+        if(person.yearOfDeath === undefined) {
+           const date = new Date();
+           person.yearOfDeath = date.getFullYear();           
+        }       
+        return (person.yearOfDeath - person.yearOfBirth) > (acc.yearOfDeath - acc.yearOfBirth) ? person : acc;
+        console.log(person); 
+    }, people[0]);
+  return oldestPerson;      
 };
+
+//console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
